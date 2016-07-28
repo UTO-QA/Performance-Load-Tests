8cb22bed8d32c5d9578aaf07b743b4f5d194e87b
braddy <baryasom@asu.edu>
2016-07-28 15:33:11 -0700

    additions to make issearch robust braddy 07/28/2016

--- Isearch/FilterLogs.py
+++ Isearch/FilterLogs.py
@@ -1,17 +1,20 @@
 import re
-
-regex = "^(uri=\")"
-dest='/home/bhaddy-linux/Downloads/isearchlog_uri_1.log'
-log='/home/bhaddy-linux/Downloads/isearchlog.txt'
-g = open(dest, 'a')
-f = open(log, 'r')
-last="\n"
-for line in f:
-    word = line.split()
-    uri = word[8].split("uri=\"")
-    urireplace = uri[1].strip("\"")+'\n'
-    print urireplace
-    g.write(urireplace)
+itit=raw_input("enter the number of iterations")
+for i in range(0,int(itit)):
+    regex = "^(uri=\")"
+    dest='/home/bhaddy-linux/Downloads/isearchlog_uri_1_itit.log'
+    log='/home/bhaddy-linux/Downloads/isearchlog.txt'
+    g = open(dest, 'a')
+    f = open(log, 'r')
+    last="\n"
+    for line in f:
+        word = line.split()
+        uri = word[8].split("uri=\"")
+        urireplace = uri[1].strip("\"")+'\n'
+        #print urireplace
+        g.write(urireplace)
+    print i
+    i=i+1
 print("*******completed***********")
 g.close()
 f.close()
--- Isearch/threadsforhttperf.py
+++ Isearch/threadsforhttperf.py
@@ -1,9 +1,18 @@
-
+# Script to combine single-sign module with httperf for iseaarchloadtesting
+'''
+    Author:Bharadwaj (braddy)
+    email:baryasom@asu.edu
+    Instructions to execute :
+    1. Need to have Following files
+            ASULogin.py,FilterLogs.py,httpe_selenium.py,,user_input.py
+    2. Need to Linux Machine for Httperf
+'''
 from subprocess import call
 import threading
 from httpe_selenium import getselenium
 
 class httperf_test(threading.Thread):
+
     def __init__(self, name,sessionvalue):
         self.sessionvalue=sessionvalue
         threading.Thread.__init__(self)
@@ -16,17 +25,27 @@ class httperf_test(threading.Thread):
 
     def connect(self):
         print "starting"
-        call(
-            " httperf --add-header 'Cookie:SSESSd0b53dd39734cedf16e25bb4ade4ff0b=" + self.sessionvalue + ";SSONAME=Bharadwaj\n' "
-                                                                                                         "--ssl --server isearch.asu.edu  --wsesslog 1,0,/home/bhaddy-linux/isearchlog_uri_1.log",
-            shell=True)
+        if(self.sessionvalue!="novalue"):
+            print "inlogin"
+            call(
+                " httperf --add-header 'Cookie:SSESSd0b53dd39734cedf16e25bb4ade4ff0b=" + self.sessionvalue + ";SSONAME=Bharadwaj\n' --ssl --server isearch.asu.edu  --wsesslog 1,0,/home/bhaddy-linux/isearchlog_uri_2.log ",shell=True)
+        else:
+            call(" httperf --ssl --server isearch.asu.edu  --wsesslog 1,0,/home/bhaddy-linux/isearchlog_uri_3.log",shell=True)
+
         print "finished"
 
-create_session=getselenium()
-value=create_session.connect_selenium()
-print value
+value=""
 length = raw_input("Enter the number of threads")
 threads = []
+userlogin=raw_input("DO you want to login or Proceed Without login yes/no")
+if userlogin=="yes":
+    create_session = getselenium()
+    value = create_session.connect_selenium()
+    print value
+else:
+    print"not logging in"
+    value="novalue"
+
 for i in range(0, int(length)):
     thread = httperf_test(str(i),value)
     threads += [thread]
